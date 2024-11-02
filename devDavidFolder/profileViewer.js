8782aeac0dbe269921c7226ad316018849207337
neacsudavid22 <ndavid2003.dn@gmail.com>
2024-11-02 16:02:47 +0200

    take the button bozzo

--- devDavidFolder/profileViewer.js
+++ devDavidFolder/profileViewer.js
@@ -10,13 +10,23 @@ function generateForms(){
         let iframeContainer = document.getElementById("iframeContainer");
         iframeContainer.appendChild(iframeElement); // Appends iframe to body
         openForms = false;
-
     }
     else{
         const iframeElement = document.querySelector("iframe");
         iframeElement.remove();
         openForms = true;
     }
+}
+
+function seeData(){
+    const name = document.getElementByIdid("name");
+    const username = document.getElementByIdid("username");
+    const email = document.getElementByIdid("email");
+    const phone = document.getElementByIdid("phone");
+    const age = document.getElementByIdid("age");
+    const reputation = document.getElementByIdid("reputation");
+    const role = document.getElementByIdid("role");
+    //const data_creare_cont = document.getElementByIdid("data_creare_cont");
 }
 
 function app(){
